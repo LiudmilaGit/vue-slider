5d1b9203139fac4af6b267569b886c0c0d32a6be
Liudmyla Isaieva <liudmyla.fotos.isaieva@gmail.com>
2022-04-12 17:49:20 +0300

    initial commit.

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item">
+          <router-link class="navbar-link" :to="{name: 'Home'}">Home</router-link>
+        </li>
+        <li class="navbar-item">
+          <router-link class="navbar-link" :to="{name: 'About' }">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="navbar">
+    <div class="container">
+      <div class="navbar-content">
+        <a href="/" class="navbar-logo">Logo</a>
+        <ul class="navbar-list">
+          <li class="navbar-item">
+            <router-link class="navbar-link" :to="{name: 'Home'}">Home</router-link>
+          </li>
+          <li class="navbar-item">
+            <router-link class="navbar-link" :to="{name: 'About' }">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
